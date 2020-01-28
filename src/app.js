287db35d1b2ae50bf42ecee7e809c2fc11459d64
v-glb <v.networks@posteo.de>
2020-01-28 23:25:36 +0100

    Change color of blinkstick in real time

--- src/app.js
+++ src/app.js
@@ -3,14 +3,29 @@ const blinkstick = require('blinkstick');
 
 let currentColor;
 
-// Find connected blinkstick via USB
+// Find connected led via USB
 const led = blinkstick.findFirst();
+
+// Get led information
+led.getDescription((err, data) => {
+  document.getElementById('device-info-h').innerHTML += data;
+});
+
+led.getSerial((err, data) => {
+  document.getElementById('device-serial-h').innerHTML += data;
+});
+
+led.getManufacturer((err, data) => {
+  document.getElementById('device-manufact-h').innerHTML += data;
+});
+
 
 // Handle color selection
 aColorPicker.from('.picker')
   .on('change', (picker, color) => {
     document.body.style.backgroundColor = color;
     currentColor = aColorPicker.parseColor(color, 'hex')
+    led.setColor(currentColor);
   });
 
 // Submit color to blinkstick
