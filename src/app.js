e2f9c8cc15a341bcea9dfe234a6bb0e88a7a9ed3
v-glb <v.networks@posteo.de>
2020-02-19 23:54:37 +0100

    basic functionality: display information

--- src/app.js
+++ src/app.js
@@ -1,38 +1,188 @@
+// Imports 
 const aColorPicker = require('a-color-picker');
 const blinkstick = require('blinkstick');
+const si = require('systeminformation');
+const electron = require('electron');
+const { ipcRenderer } = electron;
 
+// TODO: Define DOM elements as object / variables
+
+// Save state of currentColor with aColorPicker
 let currentColor;
+// Save state wether CPU or RAM usage is being monitored
+let currentMonitor;
 
-// Find connected led via USB
+// Find first connected led via USB
 const led = blinkstick.findFirst();
 
-// Get led information
-led.getDescription((err, data) => {
-  document.getElementById('device-info-h').innerHTML += data;
+if (!led) {
+  console.log('Please check USB!');
+  document.getElementById('nav-device-title').innerHTML = 'No LED!';
+
+} else {
+  // Get led information
+  led.getDescription((err, data) => {
+    document.getElementById('nav-device-title').innerHTML += data;
+  });
+
+  led.getSerial((err, data) => {
+    document.getElementById('device-serial-h').innerHTML += data;
+  });
+
+  led.getManufacturer((err, data) => {
+    document.getElementById('device-manufact-h').innerHTML += data;
+  });
+
+}
+
+/* 
+        EVENT LISTENERS
+*/
+
+// Initial State: Watch CPU usage
+window.addEventListener('DOMContentLoaded', event => {
+  monitorCPU();
 });
 
-led.getSerial((err, data) => {
-  document.getElementById('device-serial-h').innerHTML += data;
+
+// Enabled / Disable monitoring
+document.getElementById('toggle-monitor').addEventListener('click', event => {
+  const monStatus = document.getElementById('current-monitor').innerHTML;
+
+  if (monStatus.includes('CPU') || monStatus.includes('RAM')) {
+    stopMonitoring();
+    document.getElementById('current-monitor').innerHTML = 'Monitoring: Disabled!';
+    // TODO: Send ipc to main process to grey out menu items for cpu and ram monitoring
+    // ipcRenderer.send
+  } else {
+    document.getElementById('current-monitor').innerHTML = 'Please choose monitor from menu';
+  }
+
 });
 
-led.getManufacturer((err, data) => {
-  document.getElementById('device-manufact-h').innerHTML += data;
-});
-
-
-// Handle color selection
+/* 
+        GLORIOUS COLOR WHEEL
+*/
 aColorPicker.from('.picker')
   .on('change', (picker, color) => {
     document.body.style.backgroundColor = color;
     currentColor = aColorPicker.parseColor(color, 'hex')
     led.setColor(currentColor);
+
+    // Stop monitoring if user picks custom color
+    clearTimeout(currentMonitor);
   });
 
-// Submit color to blinkstick
-document.querySelector('.color-change').addEventListener('click', () => {
-  console.log('asdfasdf');
-  // Get current color's hex value
-  led.morph(currentColor, () => {
-    console.log(`Changing my color to ${currentColor}! Woohoo!`);
-  });
+// Close app on click of 'x'
+document.getElementById('close-btn').addEventListener('click', () => {
+  ipcRenderer.send('app:close');
+});
+
+/* 
+------------------ RAM Monitor ------------------------
+Tresholds:
+  Warning   - 50% Usage
+  Critical  - 80% Usage
+*/
+
+let memWarn;
+let memCrit;
+
+function monitorMem() {
+  // Get current memory information as promise
+  si.mem()
+    // Consume promise
+    .then(data => {
+      // Calculate values based on total RAM
+      memWarn = data.total * 0.5;
+      memCrit = data.total * 0.8;
+
+      const memUsage = Math.round((data.active / data.total) * 100 * 100) / 100;
+      document.getElementById('current-monitor').innerHTML = 'Monitoring: RAM';
+      document.getElementById('current-monitor-use').innerHTML = `Usage: ${memUsage}%`;
+
+      // Turn blinkstick red and start pulsing if RAM is critical 
+      if (data.active >= memCrit) {
+        console.log('Critical mem usage!');
+        led.pulse('red', () => {
+          led.morph('red')
+        });
+      }
+
+      // Turn blinkstick orange if RAM is warning
+      else if (data.active >= memWarn) {
+        led.morph('orange');
+
+        // Turn blinkstick green if RAM is ok
+      } else {
+        led.morph('green');
+      }
+    })
+    // Error handling
+    .catch(error => console.log(error));
+
+  // Update mem status every 5 seconds
+  currentMonitor = setTimeout(monitorMem, 5000)
+}
+
+/* 
+------------------ CPU/Average Load Monitor ------------------------
+Tresholds:
+  Warning   - 50% Usage
+  Critical  - 80%  Usage
+*/
+
+const cpuWarn = 50;
+const cpuCrit = 80;
+
+function monitorCPU() {
+  si.currentLoad()
+    .then(data => {
+      const cpuUsage = Math.round(data.currentload * 100) / 100;
+      document.getElementById('current-monitor').innerHTML = 'Monitoring: CPU';
+      document.getElementById('current-monitor-use').innerHTML = `Usage: ${cpuUsage}%`;
+
+      if (data.currentload >= cpuCrit) {
+        console.log('Critical cpu usage!');
+        led.pulse('red', () => {
+          led.morph('red')
+        });
+      }
+
+      else if (data.currentload >= cpuWarn) {
+        led.morph('orange')
+      }
+
+      else {
+        led.morph('green');
+      }
+    })
+    // Error handling
+    .catch(error => console.log(error));
+
+  currentMonitor = setTimeout(monitorCPU, 5000)
+}
+
+// Clear monitoring loops
+function stopMonitoring() {
+  clearTimeout(currentMonitor);
+}
+
+/* 
+        IPC MANAGEMENT
+*/
+ipcRenderer.on('monitor:ram', e => {
+  // Clear previous monitor
+  clearTimeout(currentMonitor);
+
+  console.log('Keeping an eye on memory usage.');
+  monitorMem();
+});
+
+ipcRenderer.on('monitor:cpu', e => {
+  // Clear previous monitor
+  clearTimeout(currentMonitor);
+
+  console.log('Keeping an eye on cpu usage.');
+  monitorCPU();
 });
