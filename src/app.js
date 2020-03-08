26e15aa3fc45a4d5d96c637ec704b9084d1d1fac
v-glb <v.networks@posteo.de>
2020-03-08 19:48:05 +0100

    Refactoring and cleaning up structure

--- src/app.js
+++ src/app.js
@@ -12,17 +12,84 @@ const DOMelements = {
   closeBtn: document.getElementById('close-btn'),
   currentMonitor: document.getElementById('current-monitor'),
   currentMonitorUse: document.getElementById('current-monitor-use'),
+  blinkstickStatus: document.getElementById('blinkstick-status'),
   currentMonitorPause: document.getElementById('current-monitor-pause'),
   colorPicker: document.getElementById('color-picker'),
   darkModeSwitchStatus: document.getElementById('dark-mode-switch-status')
 }
 
+// Save state of currentColor with aColorPicker
+let currentColor;
+// Save state wether CPU or RAM usage is being monitored
+let currentMonitor;
+
+// Find first connected led via USB
+const led = blinkstick.findFirst();
+
+if (!led) {
+  console.log('Please check USB!'); // Debug, remove later
+  DOMelements.navDeviceTitle.innerHTML = 'No LED!';
+
+} else {
+  // Get led information
+  led.getDescription((err, data) => {
+    DOMelements.navDeviceTitle.innerHTML += data;
+  });
+
+  led.getSerial((err, data) => {
+    DOMelements.deviceSerial.innerHTML += data;
+  });
+}
+
 /* 
-        EVENT LISTENERS
+        EVENT LISTENERS START
 */
+
+// Close app on click of 'x'
+DOMelements.closeBtn.addEventListener('click', () => {
+  ipcRenderer.send('app:close');
+});
 
 // Apply dark-theme css
 DOMelements.darkModeSwitchStatus.addEventListener('change', v => {
+  darkModeToggler();
+});
+
+window.addEventListener('DOMContentLoaded', e => {
+  // Initialize modal trigger
+  const elems = document.querySelectorAll('.modal');
+  const instances = M.Modal.init(elems);
+});
+
+// Initial State: Watch CPU usage
+window.addEventListener('DOMContentLoaded', event => {
+  monitorCPU();
+});
+
+/* 
+        EVENT LISTENERS END
+*/
+
+/* 
+        GLORIOUS COLOR WHEEL START
+*/
+
+aColorPicker.from('.picker')
+  .on('change', (picker, color) => {
+    currentColor = aColorPicker.parseColor(color, 'hex')
+    led.setColor(currentColor);
+  });
+
+/* 
+        GLORIOUS COLOR WHEEL END
+*/
+
+/* 
+        FUNCTIONS START
+*/
+
+// Toggle bewteen light/dark mode
+function darkModeToggler() {
   document.querySelector('body').classList.toggle('dark-theme-bg');
   document.querySelector('.modal').classList.toggle('dark-theme-bg');
   document.querySelector('.modal-footer').classList.toggle('dark-theme-bg');
@@ -47,62 +114,8 @@ DOMelements.darkModeSwitchStatus.addEventListener('change', v => {
   thList.forEach((el) => {
     el.classList.toggle('dark-theme-font');
   });
-});
 
-
-window.addEventListener('DOMContentLoaded', e => {
-  // Initialize modal trigger
-  const elems = document.querySelectorAll('.modal');
-  const instances = M.Modal.init(elems);
-});
-
-// Save state of currentColor with aColorPicker
-let currentColor;
-// Save state wether CPU or RAM usage is being monitored
-let currentMonitor;
-// switch for showing/hiding color picker
-let colorSwitchStatus;
-
-// Find first connected led via USB
-const led = blinkstick.findFirst();
-
-if (!led) {
-  console.log('Please check USB!'); // Debug, remove later
-  DOMelements.navDeviceTitle.innerHTML = 'No LED!';
-
-} else {
-  // Get led information
-  led.getDescription((err, data) => {
-    DOMelements.navDeviceTitle.innerHTML += data;
-  });
-
-  led.getSerial((err, data) => {
-    DOMelements.deviceSerial.innerHTML += data;
-  });
 }
-
-/* 
-        EVENT LISTENERS
-*/
-
-// Initial State: Watch CPU usage
-window.addEventListener('DOMContentLoaded', event => {
-  monitorCPU();
-});
-
-/* 
-        GLORIOUS COLOR WHEEL
-*/
-aColorPicker.from('.picker')
-  .on('change', (picker, color) => {
-    currentColor = aColorPicker.parseColor(color, 'hex')
-    led.setColor(currentColor);
-  });
-
-// Close app on click of 'x'
-DOMelements.closeBtn.addEventListener('click', () => {
-  ipcRenderer.send('app:close');
-});
 
 /* 
 ------------------ RAM Monitor ------------------------
@@ -119,16 +132,19 @@ function monitorMem() {
   si.mem()
     // Consume promise
     .then(data => {
+      DOMelements.currentMonitorPause.innerHTML = '';
+
       // Calculate values based on total RAM
       memWarn = data.total * 0.5;
       memCrit = data.total * 0.8;
 
       const memUsage = Math.round((data.active / data.total) * 100 * 100) / 100;
       DOMelements.currentMonitor.innerHTML = 'RAM';
-      DOMelements.currentMonitorUse.innerHTML = `${memUsage}%`
+      DOMelements.currentMonitorUse.innerHTML = `${memUsage}%`;
 
       // Turn blinkstick red and start pulsing if RAM is critical 
       if (data.active >= memCrit) {
+        DOMelements.blinkstickStatus.innerHTML = 'Critical';
         console.log('Critical mem usage!'); // Debug, remove later
         led.pulse('red', () => {
           led.morph('red')
@@ -137,10 +153,12 @@ function monitorMem() {
 
       // Turn blinkstick orange if RAM is warning
       else if (data.active >= memWarn) {
+        DOMelements.blinkstickStatus.innerHTML = 'Warning';
         led.morph('orange');
 
         // Turn blinkstick green if RAM is ok
       } else {
+        DOMelements.blinkstickStatus.innerHTML = 'OK';
         led.morph('green');
       }
     })
@@ -164,11 +182,14 @@ const cpuCrit = 80;
 function monitorCPU() {
   si.currentLoad()
     .then(data => {
+      DOMelements.currentMonitorPause.innerHTML = '';
+
       const cpuUsage = Math.round(data.currentload * 100) / 100;
       DOMelements.currentMonitor.innerHTML = 'CPU';
       DOMelements.currentMonitorUse.innerHTML = `${cpuUsage}%`;
 
       if (data.currentload >= cpuCrit) {
+        DOMelements.blinkstickStatus.innerHTML = 'Critical';
         console.log('Critical cpu usage!'); //Debug remove later
         led.pulse('red', () => {
           led.morph('red')
@@ -176,10 +197,12 @@ function monitorCPU() {
       }
 
       else if (data.currentload >= cpuWarn) {
+        DOMelements.blinkstickStatus.innerHTML = 'Warning';
         led.morph('orange')
       }
 
       else {
+        DOMelements.blinkstickStatus.innerHTML = 'OK';
         led.morph('green');
       }
     })
@@ -203,11 +226,17 @@ function stopMonitoring() {
 function clearStatusTable() {
   DOMelements.currentMonitor.innerHTML = '';
   DOMelements.currentMonitorUse.innerHTML = '';
+  DOMelements.blinkstickStatus.innerHTML = '';
 }
 
 /* 
-        IPC MANAGEMENT
+        FUNCTIONS END
 */
+
+/* 
+        IPC MANAGEMENT START
+*/
+
 ipcRenderer.on('monitor:ram', e => {
   // Clear previous monitor
   clearTimeout(currentMonitor);
@@ -227,3 +256,7 @@ ipcRenderer.on('monitor:cpu', e => {
 ipcRenderer.on('monitor:pause', e => {
   stopMonitoring();
 });
+
+/*
+        IPC MANAGEMENT END
+*/
